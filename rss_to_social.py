ceaa2b4774beb8d453fbc65b4387855d1ffa8dd7
Data Lab Tech <204358040+DataLabTechTV@users.noreply.github.com>
2025-05-26 17:34:38 +0100

    fix: corrected way to check media content existence

--- rss_to_social.py
+++ rss_to_social.py
@@ -120,7 +120,7 @@ class Post:
 
         post.link = entry.link
 
-        if entry.media_content is not None and len(entry.media_content) > 0:
+        if "media_content" in entry and len(entry.media_content) > 0:
             img_url = entry.media_content[0].get("url")
 
             if img_url is not None:
